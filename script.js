f79435ac054f9468d05652228d10997cdc3f4630
rafaelarrocha <145746078+rafaelarrocha@users.noreply.github.com>
2024-02-16 00:06:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const painel = document.getElementById('container')
+const cadastroBtn = document. getElementById('cadastro');
+const loginBtn = document. getElementById('login')
+    
+    cadastroBtn.addEventListener('click', () => {
+     container.classList.add("active");
+});
+    
+    loginBtn.addEventListener('click', () =>{
+        container.classList.remove("active");
+    })
